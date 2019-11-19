0df5d618209b8667a036430f81353daaa021d8f8
OrenBen-Meir <orenbm11@gmail.com>
2019-11-18 23:11:05 -0500

    dropped id in dataframes and fixed error callback

--- api/doctor/make_doctor.py
+++ api/doctor/make_doctor.py
@@ -15,46 +15,39 @@ print('conditions :\n', conditions, '\n')
 print('subj_sym :\n', subj_sym, '\n')
 print('diagnosis :\n', diagnosis, '\n')
 
+symptoms_list = [sym_entry['symptom_name'] for sym_entry in symptoms] # list of symptoms
+conditions_list = [con_entry['condition_name'] for con_entry in conditions] # list of conditions
+
 ids = {"id": [subj["id"] for subj in subjects]}  # dictionary of ids
+num_subjects = len(ids['id'])
 
-subj_sym_dic = ids.copy()  # dictionary of subjects and their symptoms
-subj_diag_dic = ids.copy()  # dictionary of subjects and their diagnosis
+subj_symptoms_dic = ids.copy()  # dictionary of subjects and their symptoms
+diagnosis_dic = ids.copy()  # dictionary of subjects and their diagnosis
 
-for symptm in symptoms:  # create an uninitialized dictionary for subjects and symptoms
-    subj_sym_dic[symptm['symptom_name']] = len(ids["id"])*[0]
+for sym in symptoms_list:  # create an uninitialized dictionary for subjects and symptoms
+    subj_symptoms_dic[sym] = num_subjects*[0]
 
-for condn in conditions:  # create an uninitialized dictionary for diagnosis
-    subj_diag_dic[condn['condition_name']] = len(ids["id"])*[0]
+for con in conditions_list:  # create an uninitialized dictionary for diagnosis
+    diagnosis_dic[con] = num_subjects*[0]
 
-# dataframe of subject's symptoms
-subj_sym_df = pd.DataFrame(data=subj_sym_dic)
-
-# data frame for diagnosis
-subj_diag_df = pd.DataFrame(data=subj_diag_dic)
+subj_symptoms_df = pd.DataFrame(data=subj_symptoms_dic) # dataframe of subject's symptoms
+diagnosis_df = pd.DataFrame(data=diagnosis_dic) # data frame for diagnosis
 
 
 for case in subj_sym:  # initialize dataframe of subject's symptoms
-
-    # id of subject
-    subj_id = case['subject_id']
-
-    # index of subject
-    index = subj_sym_df.index[subj_sym_df['id'] == subj_id].tolist()[0]
-
-    # set intensity of subject's symptom
-    subj_sym_df.iloc[index][case['symptom_name']] = case['intensity']
+    subj_id = case['subject_id'] # id of subject
+    index = subj_symptoms_df.index[subj_symptoms_df['id'] == subj_id].tolist()[0] # index of subject
+    subj_symptoms_df.iloc[index][case['symptom_name']] = case['intensity'] # set intensity of subject's symptom
 
 for case in diagnosis:  # initialize dataframe of subject's diagnosis
+    subj_id = case['subject_id'] # id of subject
+    index = diagnosis_df.index[diagnosis_df['id'] == subj_id].tolist()[0] # index of subject
+    diagnosis_df.iloc[index][case['condition_name']] = 1 # sets a condition for the subject
 
-     # id of subject
-    subj_id = case['subject_id']
+# Dropping id for both dataframes as they are no longer needed
+subj_symptoms_df.drop(columns=['id'], inplace=True)
+diagnosis_df.drop(columns=['id'], inplace=True)
 
-    # index of subject
-    index = subj_diag_df.index[subj_diag_df['id'] == subj_id].tolist()[0]
-
-    # sets a condition for the subject
-    subj_diag_df.iloc[index][case['condition_name']] = 1
-
-print(subj_sym_df)
-print(subj_diag_df)
+print(subj_symptoms_df)
+print(diagnosis_df)
 print("done!")
