97b9f8e56fdccb710520fa648e403c9ae8e2ab22
OrenBen-Meir <orenbm11@gmail.com>
2019-11-19 19:09:03 -0500

    clarified id in dataframe

--- api/doctor/make_doctor.py
+++ api/doctor/make_doctor.py
@@ -14,8 +14,8 @@ diagnosis = json.loads(sys.argv[5])
 symptoms_list = [sym_entry['symptom_name'] for sym_entry in symptoms] # list of symptoms
 conditions_list = [con_entry['condition_name'] for con_entry in conditions] # list of conditions
 
-ids = {"id": [subj["id"] for subj in subjects]}  # dictionary of ids
-num_subjects = len(ids['id'])
+ids = {"subject_id": [subj["id"] for subj in subjects]}  # dictionary of the id of subjects 
+num_subjects = len(ids['subject_id'])
 
 subj_symptoms_dic = ids.copy()  # dictionary of subjects and their symptoms
 diagnosis_dic = ids.copy()  # dictionary of subjects and their diagnosis
@@ -32,20 +32,20 @@ diagnosis_df = pd.DataFrame(data=diagnosis_dic) # data frame for diagnosis
 
 for case in subj_sym:  # initialize dataframe of subject's symptoms
     subj_id = case['subject_id'] # id of subject
-    index = subj_symptoms_df.index[subj_symptoms_df['id'] == subj_id].tolist()[0] # index of subject
+    index = subj_symptoms_df.index[subj_symptoms_df['subject_id'] == subj_id].tolist()[0] # index of subject
     subj_symptoms_df.iloc[index][case['symptom_name']] = case['intensity'] # set intensity of subject's symptom
 
 for case in diagnosis:  # initialize dataframe of subject's diagnosis
     subj_id = case['subject_id'] # id of subject
-    index = diagnosis_df.index[diagnosis_df['id'] == subj_id].tolist()[0] # index of subject
+    index = diagnosis_df.index[diagnosis_df['subject_id'] == subj_id].tolist()[0] # index of subject
     diagnosis_df.iloc[index][case['condition_name']] = 1 # sets a condition for the subject
 
 # Dropping id for both dataframes as they are no longer needed
-subj_symptoms_df.drop(columns=['id'], inplace=True)
-diagnosis_df.drop(columns=['id'], inplace=True)
+subj_symptoms_df.drop(columns=['subject_id'], inplace=True)
+diagnosis_df.drop(columns=['subject_id'], inplace=True)
 
 symptoms_array = np.array(subj_symptoms_df) # converts subject symptoms dataframe into a numpy array
-diagnosis_array = np.array(diagnosis_df) # converts diagnosis data into a numpy array
+diagnosis_array = np.array(diagnosis_df) # converts diagnosis dataframe into a numpy array
 
 my_doctor = Doctor() # creates a doctor object, the object can be thought of as the AI we train
 my_doctor.train(symptoms_array, diagnosis_array, symptoms_list, conditions_list) # feed the doctor training data
