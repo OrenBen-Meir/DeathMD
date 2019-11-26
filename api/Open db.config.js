2fba5e1320486d84fe559c5548d46f1544a6197d
OrenBen-Meir <orenbm11@gmail.com>
2019-11-26 15:53:33 -0500

    set dep con limit to 100

--- api/Open db.config.js
+++ api/Open db.config.js
@@ -2,5 +2,6 @@ module.exports = {
   HOST: "us-cdbr-iron-east-05.cleardb.net",
   USER: "bfe1060980b5c8",
   PASSWORD: "0e5498a0",
-  DB: "heroku_77f6ffd1b6610b2"
+  DB: "heroku_77f6ffd1b6610b2",
+  CONNECTIONS: 100
 };
