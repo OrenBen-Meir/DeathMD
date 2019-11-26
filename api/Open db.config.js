c1d98c3c17b4c772c1b7d912df71076a8745b10e
OrenBen-Meir <orenbm11@gmail.com>
2019-11-26 01:57:38 -0500

    redeploy heroku

--- api/Open db.config.js
+++ api/Open db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: "us-cdbr-iron-east-05.cleardb.net",
-  USER: "b0b30652793248",
-  PASSWORD: "acd07bdc",
-  DB: "heroku_09e7401486524e1"
+  USER: "bfe1060980b5c8",
+  PASSWORD: "0e5498a0",
+  DB: "heroku_77f6ffd1b6610b2"
 };
